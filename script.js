38e0e02806ea847830d3c28bf103b1a17acde3cf
Thais Costa <tc.almeida15@gmail.com>
2018-08-19 18:29:40 -0300

    adicionar modal de login

---
+++ script.js
@@ -0,0 +1,14 @@
+let entrar = document.querySelector(".entrar");
+let modalLogin = document.querySelector("#modalLogin");
+let botaoEntrar = document.querySelector(".botaoEntrar");
+
+entrar.addEventListener("click", function() {
+    modalLogin.style.display = "flex";
+    
+});
+
+// botaoEntrar.addEventListener("click" function {
+//     modalLogin.style.display = "none";
+// });
+
+
